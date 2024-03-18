c522f0d5622a18c3434089f01a534f728d9ff6ce
Bett <bettpatrick080@gmail.com>
2024-03-19 01:11:59 +0300

    Completed Assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self,search_word):
+        self.search_word = search_word
+    def match(self,possible_anagrams):
+        anagrams = []
+        for word in possible_anagrams:
+            if sorted(self.search_word.lower()) == sorted(word) and possible_anagrams != 0:
+                anagrams.append(word)
+        return anagrams
+
+
+            
+        
